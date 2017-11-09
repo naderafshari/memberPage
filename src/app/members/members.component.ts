91332b8525c86dafe490a2994116b4f7d84b05c7
nader <nader.afshari.usa@gmail.com>
2017-11-08 22:58:20 -0800

    added database read

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -19,22 +19,39 @@ export class MembersComponent implements OnInit {
   email: string;
 
   constructor(public af: AngularFire,private router: Router) {
+    
+    var Db = firebase.database();
+
     this.af.auth.subscribe(auth => {
       if(auth) {
         this.name = auth;
+        //console.log(this.name.auth.email);
       }
     });
+    //var userId = firebase.auth().currentUser.uid;
+
+    Db.ref('members/' + this.name.uid).once('value').then(function(snapshot) {
+      //const firstName = (snapshot.val() && snapshot.val().firstname) || 'Anonymous';
+      const user = snapshot.val();
+      //console.log(user);
+      this.firstName = user.firstname;
+      this.lastName = user.lastname;
+      this.email = user.email;
+    }.bind(this));
+
   }
 
   addMember() {
     var Db = firebase.database();
 
-    Db.ref('members/' + this.lastName).set({   //make the id unique somehow
+    Db.ref('members/' + this.name.uid).set({   //make the id unique somehow
       firstname: this.firstName,
       lastname: this.lastName,
       email: this.email,
     });
-  }
+    this.af.auth.logout();
+    this.router.navigateByUrl('/login');
+ }
 
   logout() {
      this.af.auth.logout();
